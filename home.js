ca761a9a983de9faf487492480743f06bea8f6da
MuhammadTaha110 <139653845+MuhammadTaha110@users.noreply.github.com>
2024-04-27 00:33:41 +0500

    Added

---
+++ home.js
@@ -0,0 +1,23 @@
+const mainColor = '#f9b623';
+const bgColor = '#feebc0';
+
+const home = document.createElement('div');
+home.className = 'home';
+
+const heading = document.createTextNode('Home');
+home.appendChild(heading);
+
+home.style.width ='100%';
+home.style.backgroundColor = bgColor;
+home.style.textAlign='center';
+document.body.appendChild(home);
+
+const homeHeadDiv = document.createElement('div');
+home.style.width ='80%';
+home.style.height='150px'
+home.style.backgroundColor = mainColor;
+home.appendChild(homeHeadDiv);
+home.
+
+console.warn(home);
+
