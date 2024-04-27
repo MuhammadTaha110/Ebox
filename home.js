cc50f2fcbf8ff820c028de6235283058bcf3a156
MuhammadTaha110 <139653845+MuhammadTaha110@users.noreply.github.com>
2024-04-28 01:44:33 +0500

    Update home.js

--- home.js
+++ home.js
@@ -38,7 +38,7 @@ const heading = document.createTextNode('Home');
 //* Append the text node to the heading wrapper
 headingWrapper.appendChild(heading);
 //* Apply marginTop style to the heading wrapper
-headingWrapper.style.paddingTop = '25px'; 
+headingWrapper.style.paddingTop = '25px';
 headingWrapper.style.fontWeight = '600'
 headingWrapper.style.fontSize = '1.4rem'
 // * Append the heading wrapper to the homeHeadDiv or any other parent element
@@ -63,107 +63,101 @@ sunMoon.style.boxShadow = '0 0 35px 5px yellow, 0 0 25px 10px yellow inset';
 
 // ! Product Card Container
 const cardContainer = document.createElement('div');
-    cardContainer.style.display = 'flex';
-    cardContainer.style.flexWrap = 'wrap';
-    cardContainer.style.justifyContent = 'space-around'
-    cardContainer.style.margin = '-120px auto ';
-    cardContainer.style.width = '95%';
-    //cardContainer.style.backgroundColor = 'pink'
-    home.appendChild(cardContainer);
+cardContainer.style.display = 'flex';
+cardContainer.style.flexWrap = 'wrap';
+cardContainer.style.justifyContent = 'space-around'
+cardContainer.style.margin = '-120px auto ';
+cardContainer.style.width = '95%';
+cardContainer.className = 'cardContainer';
+home.appendChild(cardContainer);
 
 
-    for (let i = 0; i < cardsQuantity; i++) {
-        createCards()
-        }
+for (let i = 0; i < cardsQuantity; i++) {
+    createCards()
+}
 // *function to create new items cards
 function createCards(cardsQuantity) {
 
-    
 
-   
+    //* Card Element
+    const card = document.createElement('div');
+    card.style.width = '260px'; // Set width here
+    card.style.height = '300px'; // Set height here
+    card.style.backgroundColor = themeColors[3];
+    card.style.borderRadius = '12px'
+    card.style.margin = '12px 5px'
+    card.className = 'card';
+    card.style.boxShadow = 'var(--shadow-color) 0px 54px 55px, var(--shadow-color) 0px -12px 30px, var(--shadow-color) 0px 4px 6px, var(--shadow-color) 0px 12px 13px, var(--shadow-color) 0px -3px 5px'
 
-        cardContainer.className = 'cardContainer';
+    //* card InnerDiv Rectangle
+    const cardInnerDivBG = document.createElement('div');
 
-        //* Card Element
-        const card = document.createElement('div');
-        card.style.width = '260px'; // Set width here
-        card.style.height = '300px'; // Set height here
+    // Set the class name of the new div element
+    cardInnerDivBG.className = 'cardInnerBG';
+    // Set the background color of the new div element
+    cardInnerDivBG.style.backgroundColor = themeColors[color];
 
-        const randomNumber = Math.random();
+      //* card InnerDiv Rectangle
+      const cardInnerCircleBG = document.createElement('div');
+      cardInnerDivBG.appendChild(cardInnerCircleBG);
 
-        // Scale the random number to fit the range [5, 8]
-        //const cardBg = Math.floor(randomNumber * 4) + 5;
+      // Set the class name of the new div element
+      cardInnerCircleBG.className = 'cardInnerCircle';
+      // Set the background color of the new div element
+      cardInnerCircleBG.style.backgroundColor = themeColors[1];
+      cardInnerCircleBG.style.width = '96px';
+      cardInnerCircleBG.style.height = '96px';
+      cardInnerCircleBG.style.borderRadius = '50%';
+      cardInnerCircleBG.style.margin = 'auto';
+      cardInnerCircleBG.style.border = '2px solid white';
 
 
-        card.style.backgroundColor = themeColors[3];
-        card.style.borderRadius = '12px'
-        card.style.margin = '12px 5px'
-        card.className = 'card';
-        card.style.boxShadow = 'var(--shadow-color) 0px 54px 55px, var(--shadow-color) 0px -12px 30px, var(--shadow-color) 0px 4px 6px, var(--shadow-color) 0px 12px 13px, var(--shadow-color) 0px -3px 5px'
-
-        const cardInnerDivBG = document.createElement('div');
-
-        // Set the class name of the new div element
-        cardInnerDivBG.className = 'cardInnerBG';
-
-        
       
-        // Set the background color of the new div element
-        cardInnerDivBG.style.backgroundColor = themeColors[color];
-
-            // * Give Colors to cardsInner BG 
-            if(color<=8){
-                color++;
-            }
-            else{
-                color=5;
-            }
-        
-    
-        // Set the width and height of the new div element
-        cardInnerDivBG.style.width = '84%';
-        cardInnerDivBG.style.borderRadius = '12px';
-        cardInnerDivBG.style.height = '100px';
-        cardInnerDivBG.style.margin = '25% auto'
 
 
 
-        // Append the new div element to the card
-        card.appendChild(cardInnerDivBG);
-
-        cardContainer.appendChild(card);
-
-        const productTittleContainer = document.createElement('div');
-        const productPriceContainer = document.createElement('div');
-
-        productTittleContainer.className = 'productTittle';
-        productPriceContainer.className = 'productPrice';
+    // * Give Colors to cardsInner BG 
+    if (color <= 8) {
+        color++;
+    }
+    else {
+        color = 5;
+    }
 
 
-        const productTittle = document.createTextNode('Nike AirMax');
-        const productPrice = document.createTextNode('$44.26');
+    // Set the width and height of the new div element
+    cardInnerDivBG.style.width = '84%';
+    cardInnerDivBG.style.borderRadius = '12px';
+    cardInnerDivBG.style.height = '100px';
+    cardInnerDivBG.style.margin = '25% auto'
 
-        productTittleContainer.style.color= themeColors[6];
-        productPriceContainer.style.color= themeColors[6];
+    // Append the new div element to the card
+    card.appendChild(cardInnerDivBG);
 
-        productTittleContainer.style.fontWeight= '800';
+    cardContainer.appendChild(card);
 
+    const productTittleContainer = document.createElement('div');
+    const productPriceContainer = document.createElement('div');
 
-
-
-        productTittleContainer.appendChild(productTittle);
-        productPriceContainer.appendChild(productPrice);
-
-
-        // TODO: Append the text node to the heading wrapper
-
-        card.appendChild(productTittleContainer);
-        card.appendChild(productPriceContainer);
+    productTittleContainer.className = 'productTittle';
+    productPriceContainer.className = 'productPrice';
 
 
-    
+    const productTittle = document.createTextNode('Nike AirMax');
+    const productPrice = document.createTextNode('$44.26');
+
+    productTittleContainer.style.fontWeight = '800';
+    productTittleContainer.style.color = themeColors[6];
+    productPriceContainer.style.color = themeColors[6];
+
+    productTittleContainer.appendChild(productTittle);
+    productPriceContainer.appendChild(productPrice);
+
+
+    // TODO: Append the text node to the heading wrapper
+    card.appendChild(productTittleContainer);
+    card.appendChild(productPriceContainer);
+
+
 }
-
-
-console.warn(home);
 
